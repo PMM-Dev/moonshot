403cca530dca3847300e00a60766b4f4ddb6f23f
Kwon770 <sckwon770@gmail.com>
2020-11-11 15:55:10 +0900

    chore: init

---
+++ Assets/2.Scripts/System/SmoothTargetFollowing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace System
+{
+    public class SmoothTargetFollowing : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _target;
+
+        [SerializeField]
+        private float _smoothSpeed = 0.125f;
+        [SerializeField]
+        private Vector3 _offeset;
+
+        private void FixedUpdate()
+        {
+            Vector3 targetPosition = _target.position + _offeset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
