3dd8bc686c7b4bd4ed3b1677bcc4933832af4f37
Kwon770 <sckwon770@gmail.com>
2020-11-12 22:24:07 +0900

    feat: Make MainEventManager as singleton

--- Assets/2.Scripts/System/MainEventManager.cs
+++ Assets/2.Scripts/System/MainEventManager.cs
@@ -5,6 +5,44 @@ using UnityEngine;
 
 public class MainEventManager : MonoBehaviour
 {
+    private static MainEventManager _instance;
+    public static MainEventManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var obj = FindObjectOfType<MainEventManager>();
+                if (obj != null)
+                {
+                    _instance = obj;
+                }
+                else
+                {
+                    var newSingleton = new GameObject("Singleton Class").AddComponent<MainEventManager>();
+                    _instance = newSingleton;
+                }
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        var objs = FindObjectsOfType<MainEventManager>();
+        if (objs.Length != 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
     MainDataManager mainEventDataManager;
     MainUIManager mainUIManager;
 
