ae0583547378e3ddd81cfcee0aa7379cfb6750e3
jsuk10 <jsuk10@naver.com>
2020-11-15 16:38:33 +0900

    chore : make a Dummy

---
+++ Assets/2.Scripts/Enemy/MeleeAttackPattern.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class MeleeAttackPattern : Pattern
+    {
+        //근접 공격 패턴 구현
+        protected override void OriginPatten()
+        {
+
+        }
+    }
+}
