98910524a013fef1361434a568f5349096e047fe
jsuk10 <jsuk10@naver.com>
2020-11-17 15:03:12 +0900

    chore : init setting
    Create "wolf pattern", Melee Pattern" inheriting "pattern"
    "pattern"을 상속하는 "wolf pattern", Melee Pattern"을 만듬

--- Assets/2.Scripts/Enemy/MeleePattern.cs
+++ Assets/2.Scripts/Enemy/MeleePattern.cs
@@ -6,10 +6,7 @@ namespace Enemy
 {
     public class MeleeAttackPattern : Pattern
     {
-        //근접 공격 패턴 구현
-        protected override void OriginPatten()
-        {
+        
 
-        }
     }
 }
