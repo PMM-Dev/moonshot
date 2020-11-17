1c3814a851a0ebff86da49576c193b645f55f919
jsuk10 <jsuk10@naver.com>
2020-11-17 23:03:10 +0900

    feat : maek a override pattern

---
+++ Assets/2.Scripts/Enemy/ExplosionkPattern.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class ExplosionkPattern : Pattern
+    {
+        override protected void ActuallyPattern()
+        {
+            if (_WayPointDistance < _patternRage)
+            {
+                //플레이어 데미지 주는 함수
+                Debug.Log("player dead");
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/2.Scripts/Enemy/MeleeAttackPattern.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class MeleeAttackPattern : Pattern
+    {
+        override protected void ActuallyPattern()
+        {
+            if (_WayPointDistance < _patternRage)
+            {
+                //플레이어 데미지 주는 함수
+                Debug.Log("player dead");
+            }
+        }
+    }
+}
