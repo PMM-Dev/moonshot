892fcc1daf64abb49d2276fbff67545398a0ca9c
jsuk10 <jsuk10@naver.com>
2020-11-17 15:04:25 +0900

    feat : make Calculation function
    Calculation distance
    계산하는 함수 만들어줌
    
    차후에 EnemyMove에서 읽을수 있게 할예정

---
+++ Assets/2.Scripts/Enemy/Pattern.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Enemy{
+    public class Pattern : MonoBehaviour
+    {
+        [SerializeField]int _pattenDelay;
+        [SerializeField] float _rage;
+        private GameObject _target;
+        float _distance;//차후에 move에서 받아오기
+
+        private void Update()
+        {
+            PhysicalCalculation();
+        }
+
+
+        //거리와, 방향을 계산하는 함수
+        void PhysicalCalculation()
+        {
+            _distance = Vector3.Magnitude(this.gameObject.transform.localPosition - _target.transform.localPosition);
+        }
+
+    }
+
+    
+}
