b998fad49c31f6cef6a46f2a6d11be1241779757
sjmjys954646 <sjmjys954646@gmail.com>
2020-11-19 15:06:36 +0900

    feat : map random loading
    Add 3 stage per map random loading system

--- Assets/2.Scripts/Map/MapMaking.cs
+++ Assets/2.Scripts/Map/MapMaking.cs
@@ -9,7 +9,9 @@ public class MapMaking : MonoBehaviour
     [SerializeField]
     private GameObject mapCandidate;
     [SerializeField]
-    private int mapYlength;
+    private float mapYlength;
+    [SerializeField]
+    private float startPointY;
     
     private int[] mapFrequency = new int[20];//not two times more selected when first random check
     private int[] mapFrequencyCheck = new int[20];// check the frequency when locating
@@ -39,16 +41,18 @@ public class MapMaking : MonoBehaviour
         for(int i = 0;i< stagePerMapCount; i++)
         {
             int idx = mapOrder[i];
-            if(mapFrequencyCheck[idx]==0)
+            Vector3 targetPosition = new Vector3(0, startPointY + mapYlength * (i + 1)+4.2f, 0);
+            Debug.Log(targetPosition);
+            if (mapFrequencyCheck[idx]==0)
             {
                 GameObject game = mapCandidate.transform.GetChild(idx).gameObject;
-                game.transform.position = new Vector3(0, mapYlength * (i+1), 0);
+                game.transform.position = targetPosition;
                 mapFrequencyCheck[idx]++;
             }
             else if(mapFrequencyCheck[idx] == 1)
             {
                 GameObject game = mapCandidate.transform.GetChild(idx+mapCount/2).gameObject;
-                game.transform.position = new Vector3(0, mapYlength * (i + 1), 0);
+                game.transform.position = targetPosition;
                 mapFrequencyCheck[idx]++;
             }
             else
