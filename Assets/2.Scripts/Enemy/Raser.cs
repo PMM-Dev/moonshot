0c352357bc9b51c95c9ff8a5aa98bf94251e45cc
jsuk10 <jsuk10@naver.com>
2020-11-20 23:51:31 +0900

    feat : make a Prefabs
    각 몬스터의 프리팹을 만듬.

---
+++ Assets/2.Scripts/Enemy/Raser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raser : MonoBehaviour
+{
+    [SerializeField] float _raserInterber;
+    [SerializeField] bool _israserVertical;
+    [SerializeField] GameObject _bullit;
+    
+
+}
