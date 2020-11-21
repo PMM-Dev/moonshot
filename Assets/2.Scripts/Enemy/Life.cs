a2ac4b82a54ece965990c7e343e85195299d7b8e
jsuk10 <jsuk10@naver.com>
2020-11-21 19:25:57 +0900

    feat : Add Life.sc

---
+++ Assets/2.Scripts/Enemy/Life.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Life : MonoBehaviour, IDamage
+    {
+        public void GetDamage()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
