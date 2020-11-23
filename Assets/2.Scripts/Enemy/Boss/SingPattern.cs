fbaf08b7ca059f498d572f2412e37ab0663e3729
jsuk10 <jsuk10@naver.com>
2020-11-24 03:04:15 +0900

    feat : SingPattern
    비활성화 된 객체를 활성화 시킴
    주의사항으로는 _masSpownCount를 높게 잡아버리면 무한루프에 빠짐...
     - 모든 객체가 활성화 되면 루프를 계속 돌기 때문

---
+++ Assets/2.Scripts/Enemy/Boss/SingPattern.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class SingPattern : Patterns, IPattern
+    {
+        [Header("안보이는 몹 할당")]
+        [SerializeField]
+        GameObject[] _rabbitPostion;
+        [SerializeField]
+        GameObject[] _wolfPostion;
+        [SerializeField]
+        GameObject[] _masPeoplePostion;
+        [Header("전체 몹값보다 충분히 낮게 잡지 않으면 겜 멈춤")]
+        [SerializeField]
+        int _maxSpownCount;
+
+        int _random;
+        private int _spwoncount;
+        
+
+        public override IEnumerator Run()
+        {
+            _spwoncount = 0;
+            while (_maxSpownCount > _spwoncount)
+            {
+                switch (Random.Range(0, 3))
+                {
+                    case 0:
+                        _random = Random.Range(0, _rabbitPostion.Length);
+                        if (_rabbitPostion[_random].activeSelf == true)
+                            continue;
+                        _rabbitPostion[_random].SetActive(true);
+                        break;
+                    case 1:
+                        _random = Random.Range(0, _wolfPostion.Length);
+                        if (_wolfPostion[_random].activeSelf == true)
+                            continue;
+                        _wolfPostion[_random].SetActive(true);
+                        break;
+                    case 2:
+                        _random = Random.Range(0, _masPeoplePostion.Length);
+                        if (_masPeoplePostion[_random].activeSelf == true)
+                            continue;
+                        _masPeoplePostion[_random].SetActive(true);
+                        break;
+                }
+                yield return new WaitForSeconds(1);
+                _spwoncount++;
+            }
+
+        }
+    }
+}
