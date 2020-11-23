3e6bae2ffa2a9ba74ae1e44ab81d223f513c4b70
jsuk10 <jsuk10@naver.com>
2020-11-23 23:32:07 +0900

    feat : Make Meteo
    회전함, 생성될때 플레이어를 찾아서 위치를 변경해서 내려감

---
+++ Assets/2.Scripts/Enemy/Boss/Projectile/Meteo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Meteo : Projectile
+    {
+        private void Update()
+        {
+            Rotate();
+            Run();
+        }
+
+        override protected void Pattern()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
