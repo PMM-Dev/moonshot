90b04522f4da850b133657f80f9b222c3621e0d9
jsuk10 <jsuk10@naver.com>
2020-11-23 23:28:59 +0900

    feat : Make IPattern, Pattern, PatternController
    패턴인터페이스와 패턴 컨트롤러를 만들고 패턴 인터페이스를 상속하고 추상화 시킴.

---
+++ Assets/2.Scripts/Enemy/Boss/Pattern/PatternController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public interface IPattern
+    {
+        IEnumerator Run();
+    }
+
+
+    public class PatternController : MonoBehaviour
+    {
+        [Tooltip("d")][SerializeField]
+        protected List<Patterns> _patternContainer;
+        [SerializeField]
+        protected Patterns _exhaustPatterns;
+        [SerializeField]
+        int a;
+
+        protected List<Patterns> _patternContainerCopy = new List<Patterns>();
+        protected Patterns _currentPattern;
+
+        private int _count = 0;
+        private int _random = 0;
+
+        private void Start()
+        {
+
+            Debug.Log(_patternContainer.Count);
+
+            for (int i = 0; i < _patternContainer.Count; i++)
+            {
+                Debug.Log(_patternContainer[i]);
+                _patternContainerCopy.Add(_patternContainer[i]);
+                Debug.Log(_patternContainerCopy[i]);
+            }
+            StartCoroutine(FiniteStateMachine());
+        }
+
+        void RandomCurrentPattern() {
+            if (_patternContainerCopy.Count <= 0)
+                ///    _patternContainerCopy = _patternContainer;
+                ///    
+                for (int i = 0; i < _patternContainer.Count; i++)
+                    _patternContainerCopy.Add(_patternContainer[i]);
+            else
+            {
+                _random = Random.Range(0, _patternContainerCopy.Count);
+                _currentPattern = _patternContainerCopy[_random];
+                _patternContainerCopy.RemoveAt(_random);
+            }
+        }
+
+        IEnumerator FiniteStateMachine()
+        {
+            RandomCurrentPattern();
+            while (true)
+            {
+                RandomCurrentPattern();
+                yield return StartCoroutine(_currentPattern.Run());
+                _count++;
+                if (_count >= 5) {
+                    _exhaustPatterns.Run();
+                    _count = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/2.Scripts/Enemy/Boss/Patterns.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    
+    public abstract class Patterns : MonoBehaviour, IPattern
+    {
+        [Tooltip("발사체 프리펩 넣기")]
+        [SerializeField]
+        protected GameObject _projectile;
+        [Tooltip("패턴 지속 시간")]
+        [SerializeField]
+        protected float _patternTime;
+        [Tooltip("발사체 개수")]
+        [SerializeField]
+        protected float _projectileCount;
+        [Tooltip("발사체 속도")]
+        [SerializeField]
+        protected float _projectileSpeed;
+        protected int _spwoncount = 0;
+        protected float _time = 0;
+
+        protected GameObject _player;
+
+        public abstract IEnumerator Run();
+    }
+}
