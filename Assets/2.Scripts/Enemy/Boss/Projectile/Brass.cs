651d83bce97025ab64be2b1e7192a23e1a8d9e5b
jsuk10 <jsuk10@naver.com>
2020-11-24 00:34:14 +0900

    feat : Brass
    닿으면 죽음
    평소에는 숨겼다가 돌려서 할예정

---
+++ Assets/2.Scripts/Enemy/Boss/Projectile/Brass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Brass : Projectile
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player") == true)
+            {
+                collision.gameObject.GetComponent<IDamage>().GetDamage();
+            }
+        }
+    }
+}
