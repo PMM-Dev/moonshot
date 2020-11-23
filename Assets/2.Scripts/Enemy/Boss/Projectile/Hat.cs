361ade4b58bb7bedd6bbafe09cf05b993cef3906
jsuk10 <jsuk10@naver.com>
2020-11-23 23:33:33 +0900

    feat : Hat
    모자를 만듬
    모자는 아래로 내려갈때 플레이어를 찾고
    맨밑으로 내려가면 위로 다시 올라감.
    반복후 안보이게 설정

---
+++ Assets/2.Scripts/Enemy/Boss/Projectile/Hat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Hat : Projectile
+    {
+        private bool _isDown = true;
+
+        private void Update()
+        {
+            Rotate();
+            if (_isDown == true)
+            {
+                Run();
+            }
+            else
+            {
+                ReverseRun();
+            }
+        }
+
+
+        protected void ReverseRun()
+        {
+            this.transform.Translate(Vector3.up * _projectileSpeed * Time.smoothDeltaTime, Space.World);
+            if (this.transform.position.y >= _player.transform.position.y + _triggerDistance)
+                this.gameObject.SetActive(false);
+        }
+
+        override protected void Pattern() {
+            _isDown = false;
+            SetPosition();
+        }
+
+    }
+}
