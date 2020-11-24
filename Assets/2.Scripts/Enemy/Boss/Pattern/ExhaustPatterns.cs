f0ca613951b9f029a79c1a48f470e3b104e944eb
jsuk10 <jsuk10@naver.com>
2020-11-24 16:06:26 +0900

    feat : ExhaustPattern

---
+++ Assets/2.Scripts/Enemy/Boss/Pattern/ExhaustPatterns.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class ExhaustPatterns : Patterns
+    {
+        [SerializeField][Range(1,10)]
+        private float _grogyTime = 1;
+        private BossLife _bossLife;
+
+        private void Start()
+        {
+            _bossLife = this.gameObject.GetComponent<BossLife>();
+        }
+
+        public override IEnumerator Run()
+        {
+            //지친 애니 출력
+            _bossLife.IsCanAttack = true;
+            yield return new WaitForSeconds(_grogyTime);
+            //회복 애니 출력
+            _bossLife.IsCanAttack = false;
+            
+        }
+    }
+}
