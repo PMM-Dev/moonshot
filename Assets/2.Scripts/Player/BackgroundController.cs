d21ecd9b4928ad1d0c10ecf98ccc46ab803a751a
ManaBingsu <43133819+ManaBingsu@users.noreply.github.com>
2020-11-25 23:24:27 +0900

    feat : Background controller

--- Assets/2.Scripts/Player/BackgroundController.cs
+++ Assets/2.Scripts/Player/BackgroundController.cs
@@ -6,6 +6,8 @@ using Player;
 
 public class BackgroundController : MonoBehaviour
 {
+    public static BackgroundController Instance;
+
     private const float StageHalfLength = 34.1f;
     private MapMaking _mapMaking;
     private PlayerController _playerController;
@@ -14,10 +16,36 @@ public class BackgroundController : MonoBehaviour
 
     private List<SpriteRenderer> _backgroundSpriteRenderers;
 
-    private int counter;
+    public List<Vector2> _heights;
+
+    float x;
+    float y;
+    float pastY;
+
+    private Color _color;
+
+    private int _heightIndex;
+
+    public int HeightIndex
+    {
+        get { return _heightIndex; }
+        set
+        {
+            if (_heightIndex != value)
+            {
+                pastY = y;
+
+                StartCoroutine(ChangeBackground(_heightIndex));
+                _heightIndex = value;
+            }
+        }
+    }
 
     private void Awake()
     {
+        if (Instance == null)
+            Instance = this;
+
         _mapMaking = FindObjectOfType<MapMaking>();
         if (MainPlayerManager.Instance == null)
         {
@@ -25,23 +53,46 @@ public class BackgroundController : MonoBehaviour
         }
         else
         {
-            _playerController = MainPlayerManager.Instance.Player.GetComponent<PlayerController>();
+            _playerController = FindObjectOfType<PlayerController>();
+            //_playerController = MainPlayerManager.Instance.Player.GetComponent<PlayerController>();
         }
+
+    }
+
+    private void Start()
+    {
         _backgroundSpriteRenderers = new List<SpriteRenderer>();
         for (int i = 0; i < transform.childCount; i++)
         {
             _backgroundSpriteRenderers.Add(transform.GetChild(i).GetComponent<SpriteRenderer>());
         }
+        _color = _backgroundSpriteRenderers[0].color;
+
+        _heights = new List<Vector2>();
+        _heights.Add(new Vector2(-4f, 200f));
+        _heights.Add(new Vector2(200f, 480f));
+        _heights.Add(new Vector2(480f, 900f));
     }
 
-    private void Start()
+    private void Update()
     {
-        counter = 0;
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Initialize();
+        }
+    }
 
+    public void Initialize()
+    {
         if (_mapMaking != null && _playerController != null)
         {
-
+            _heightIndex = 0;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                _backgroundSpriteRenderers[_heightIndex].color = _color;
+            }
             _mapChanger = StartCoroutine(CheckHeight());
+            StartCoroutine(ChangeHeight());
         }
     }
 
@@ -49,10 +100,29 @@ public class BackgroundController : MonoBehaviour
     {
         while (true)
         {
-            if (_playerController.transform.position.y > _mapMaking.WholeMapOrder[counter].transform.position.y)
+            if (_mapMaking != null && _playerController != null)
             {
-                StartCoroutine(ChangeBackground(counter));
-                counter++;
+                for (int i = _heightIndex; i < 3; i++)
+                {
+                    if (_playerController.transform.position.y >= _heights[i].x && _playerController.transform.position.y < _heights[i].y)
+                    {
+                        HeightIndex = i;
+                    }
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator ChangeHeight()
+    {
+        while (true)
+        {
+            if (_mapMaking != null && _playerController != null)
+            {
+                x = Mathf.Clamp(_playerController.transform.position.x / 80f, -0.8f, 0.8f);
+                y = Mathf.Clamp(5f + (_heights[_heightIndex].y - _playerController.transform.position.y) / 120f, -50f, 50f);
+                _backgroundSpriteRenderers[_heightIndex].gameObject.transform.localPosition = new Vector3(x, y, 0f);
             }
             yield return null;
         }
