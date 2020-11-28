afa891806fc5a32d281f5436edb66b30a502edd8
jsuk10 <jsuk10@naver.com>
2020-11-29 01:34:42 +0900

    feat : Die Event

--- Assets/2.Scripts/Enemy/Boss/BossLife.cs
+++ Assets/2.Scripts/Enemy/Boss/BossLife.cs
@@ -9,21 +9,36 @@ namespace Enemy
         [SerializeField]
         private Animator _patternAni;
         [SerializeField]
-        private int _bossLife = 5;
+        private int _bossLifes = 5;
         private bool _isCanAttack = false;
+        private bool _isDie = false;
         public bool IsCanAttack
         {
             get { return _isCanAttack; }
             set { _isCanAttack = value; }
         }
+        public int BossLifes
+        {
+            get { return _bossLifes; }
+        }
 
-        public void Die() {
-            if (_bossLife > 0)
+        public void Die()
+        {
+            if (_bossLifes > 0)
                 return;
+            if (_isDie == true)
+                return;
+            _isDie = true;
+            StartCoroutine(DieI());
             _patternAni.Play("Die");
+            Destroy(this.gameObject, 3f);
             Debug.Log("쥬금");
             //죽는 애니메이션 출력
             //죽은 뒤에 하는 뭐 클리어창.
+        }
+        IEnumerator DieI() {
+            yield return new WaitForSeconds(2.5f);
+            MainEventManager.Instance.ClearMainGameEvent?.Invoke();
         }
 
         public bool GetDamage()
@@ -31,9 +46,9 @@ namespace Enemy
             Debug.Log("GetDamage");
             if (_isCanAttack != true)
                 return false;
-            _bossLife--;
+            _bossLifes--;
             _patternAni.Play("Hit");
-            Debug.Log("Life 는" + _bossLife);
+            Debug.Log("Life 는" + _bossLifes);
             _isCanAttack = false;
             Die();
             return true;
