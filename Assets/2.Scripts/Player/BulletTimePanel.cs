e2226eab05746214006df8ca9e2963c56417fecf
ManaBingsu <43133819+ManaBingsu@users.noreply.github.com>
2020-11-29 00:54:07 +0900

    feat : Bullet time background

---
+++ Assets/2.Scripts/Player/BulletTimePanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletTimePanel : MonoBehaviour
+{
+    public static BulletTimePanel Instance;
+
+    public Image Panel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        Panel = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
