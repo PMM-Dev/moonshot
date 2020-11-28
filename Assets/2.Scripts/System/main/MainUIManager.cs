a71f4a385728c7dff2fdfcff4bd3bef34097007e
Kwon770 <sckwon770@gmail.com>
2020-11-29 02:53:24 +0900

    feat: Add ALL ENDING;

--- Assets/2.Scripts/System/main/MainUIManager.cs
+++ Assets/2.Scripts/System/main/MainUIManager.cs
@@ -15,12 +15,31 @@ public class MainUIManager : MonoBehaviour
     [SerializeField]
     private GameObject _gameoverPanel;
     [SerializeField]
+    private GameObject _creditPanel;
+    [SerializeField]
+    private RectTransform _creditTransform;
+    [SerializeField]
+    private GameObject _thanksPanel;
+    [SerializeField]
     private GameObject _endingPanel;
+
+    [SerializeField]
+    private float _creditTime;
+    [SerializeField]
+    private float _creditEndY;
+    [SerializeField]
+    private bool isEnding = false;
 
     [SerializeField]
     private MainGameManager _mainGameManager;
 
     private bool _isOpenOption = false;
+
+    private void Start()
+    {
+        MainEventManager.Instance.ClearMainGameEvent += StartEnding;
+        MainEventManager.Instance.ClearMainGameEvent.Invoke();
+    }
 
     private void Update()
     {
@@ -37,6 +56,20 @@ public class MainUIManager : MonoBehaviour
                 _isOpenOption = true;
             }
         }
+
+        if (isEnding)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                _creditTime /= 2;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                _creditTime *= 2;
+            }
+
+        }
     }
 
     public void OnClickStart()
@@ -47,7 +80,7 @@ public class MainUIManager : MonoBehaviour
         _mainPanel.SetActive(true);
     }
 
-    public void OnClickRestart()
+    public void OnClickQuickRestart()
     {
         SceneManager.LoadScene("RestartedMain");
     }
@@ -68,11 +101,14 @@ public class MainUIManager : MonoBehaviour
         MainEventManager.Instance.ResumeGamePlayEvent?.Invoke();
     }
 
-    public void OnClickOptionRestart()
+    public void OnClickInitRestart()
     {
-        _optionPanel.SetActive(false);
+        SceneManager.LoadScene("Main");
+    }
 
-        OnClickRestart();
+    public void OnClickQuite()
+    {
+        Application.Quit();
     }
 
     public void ShowGameoverUI()
@@ -81,8 +117,33 @@ public class MainUIManager : MonoBehaviour
         _gameoverPanel.SetActive(true);
     }
 
-    public void ShowEndingPanel()
+    public void StartEnding()
+    {
+        isEnding = true;
+        StartCoroutine(Ending());
+    }
+
+    public IEnumerator Ending()
     {
         _endingPanel.SetActive(true);
+
+        _creditPanel.SetActive(true);
+        yield return StartCoroutine(PlayCredit());
+        _creditPanel.SetActive(false);
+
+        _thanksPanel.SetActive(true);
+    }
+
+    public IEnumerator PlayCredit()
+    {
+        float originY = _creditTransform.anchoredPosition.y;
+        float t = 0f;
+        while (t <= 1f)
+        {
+            t += Time.deltaTime * (1 / _creditTime);
+            float movedY = Mathf.Lerp(originY, _creditEndY, t);
+            _creditTransform.anchoredPosition = new Vector2(0, movedY);
+            yield return null;
+        }
     }
 }
