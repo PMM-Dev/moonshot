eebf8a858b60f9a730b4c81f990cde1746244337
Kwon770 <sckwon770@gmail.com>
2020-11-30 18:07:17 +0900

    feat: Add r keylistener to quick restart and Add skipGuideText in intro

--- Assets/2.Scripts/System/Intro.cs
+++ Assets/2.Scripts/System/Intro.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class Intro : MonoBehaviour
 {
     [SerializeField]
     VideoPlayer _video;
+    [SerializeField]
+    Text _skipGuideText;
 
     [SerializeField]
     GameObject[] MainGames;
@@ -15,6 +18,11 @@ public class Intro : MonoBehaviour
     private void Awake()
     {
         _video.loopPointReached += IntroEndEvent;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(HideGuide());
     }
 
     private void Update()
@@ -36,6 +44,14 @@ public class Intro : MonoBehaviour
         {
             element.SetActive(true);
         }
+        _skipGuideText.gameObject.SetActive(false);
         gameObject.SetActive(false);
+    }
+
+    public IEnumerator HideGuide()
+    {
+        yield return new WaitForSecondsRealtime(4f);
+
+        _skipGuideText.gameObject.SetActive(false);
     }
 }
